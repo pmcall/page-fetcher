1ac99da7a142a3490f15d28572191990deadffaa
Pabs <paul.mca.ca@gmail.com>
2023-05-22 02:48:43 +0000

    Removed redundant comments

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,3 @@
-// It should take two command line arguments:
-
-// a URL
-// a local file path
-
 const request = require('request');
 const fs = require("fs");
 let sourcePage = process.argv[2]
@@ -16,9 +11,3 @@ request(sourcePage, function (error, response, body) {
     console.log(`Downloaded and saved ${body.length} bytes to ${target}`)
   });
 });
-
-
-
-
-
-
